29bf7f37bd4ecb39ab70073ae806d00294aac0cc
trinareyes <katrinajreyes@outlook.com>
2024-07-09 16:25:58 -0700

    made new classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Student[] students = parseMasterListFile("master_list.txt");
+
+        Student[] course1Students = new Student[0];
+        Student[] course2Students = new Student[0];
+        Student[] course3Students = new Student[0];
+
+        for (Student student : students) {
+            if (student.getCourse().equals("course 1")) {
+                course1Students = addStudentToArray(course1Students, student);
+            } else if (student.getCourse().equals("Course 2")){
+                course2Students = addStudentToArray(course2Students, student);
+            } else if (student.getCourse().equals("course 3")) {
+                course3Students = addStudentToArray(course3Students, student);
+            }
+
+        }
+
+
+        sortStudentsByGrade(course1Students);
+        sortStudentsByGrade(course2Students);
+        sortStudentsByGrade(course3Students);
+
+
+        writeStudentToCSV("course1.csv", course1Students);
+        writeStudentToCSV("course2.csv", course2Students);
+        writeStudentToCSV("course3.csv", course3Students);
     }
+
+    private static void writeStudentToCSV(String s, Student[] course1Students) {
+    }
+
+    private static void sortStudentsByGrade(Student[] course1Students) {
+
+    }
+
+    private static Student[] addStudentToArray(Student[] course1Students, Student student) {
+    }
+
+    private static Student[] parseMasterListFile(String file) {
+    }
+
+
 }
---
+++ src/Student.java
@@ -0,0 +1,53 @@
+public class Student {
+    private String id;
+    private String name;
+    private String course;
+    private int grade;
+
+    public Student(String id, String name, String course, int grade) {
+
+        this.id = id;
+       this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{id=" + id + ", name=" + name + ", course" + course + ", grade=" + grade + "}";
+
+    }
+}
+
